178b3dae9e48edd08a2d0e16ae866f41741dc6a4
Pafgz <pierreantoine.fagniez@gmail.com>
2022-04-08 14:08:52 +0200

    Initial Commit

---
+++ Tests/CardCarouselTests/CardCarouselTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CardCarousel
+
+final class CardCarouselTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CardCarousel().text, "Hello, World!")
+    }
+}
