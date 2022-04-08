8a01ed1eae2623f77351ea9fd448d382d68d3c53
Pafgz <pierreantoine.fagniez@gmail.com>
2022-04-08 18:59:46 +0200

    Code cleaning

--- Tests/CardCarouselTests/CardCarouselTests.swift
+++ Tests/CardCarouselTests/CardCarouselTests.swift
@@ -2,10 +2,10 @@ import XCTest
 @testable import CardCarousel
 
 final class CardCarouselTests: XCTestCase {
-    func testExample() throws {
+//    func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(CardCarousel().text, "Hello, World!")
-    }
+//        XCTAssertEqual(CardCarousel().text, "Hello, World!")
+//    }
 }
